152c414cf1dc49a0b543f17bb2902291e2da95e1
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-19 21:28:51 -0600

    Display account

--- src/wallet/components/Wallet.tsx
+++ src/wallet/components/Wallet.tsx
@@ -1,32 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Web3 from 'web3';
 
 const getAccounts = async (): Promise<string[]> => {
-  console.log('givenProvider', Web3.givenProvider);
   const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
   const accounts = await web3.eth.getAccounts();
   return accounts;
 }
 
 const Wallet = (): JSX.Element => {
-  const [accounts, setAccounts] = useState<string[]>();
+  const [state, setState] = useState<Wallet.State>({
+    accounts: [],
+    buttonText: 'Connect Wallet',
+  });
 
-  useEffect(() => {
-    async function assignAccounts() {
-      const web3Accounts = await getAccounts();
-      setAccounts(web3Accounts);
-    }
-    assignAccounts();
-  }, []);
-
-  if (accounts?.length) {
-    console.log('accounts', accounts);
-  } else {
-    console.log('accounts is undefined', accounts)
+  const onClickConnectWallet = async () => {
+    const accounts = await getAccounts();
+    const buttonText = accounts[0];
+    setState({ buttonText, accounts });
   }
+
   return (
-    <>
-    </>
+    <button onClick={onClickConnectWallet}>{state.buttonText}</button>
   );
 }
 
