4ad99e16d01fd2f3c3b1f30901a5b531fe57c810
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-26 21:54:59 -0600

    Enable ethereum (#8)
    * Call window.ethereum.enable
    
    * Add review script
    
    * Format web3

---
+++ src/wallet/web3/web3.ts
@@ -0,0 +1,9 @@
+import Web3 from 'web3';
+
+export const getWeb3 = async (): Promise<Web3> => {
+  const web3 = new Web3(Web3.givenProvider);
+  // @ts-expect-error window.ethereum is defined
+  await window.ethereum.enable();
+
+  return web3;
+};
